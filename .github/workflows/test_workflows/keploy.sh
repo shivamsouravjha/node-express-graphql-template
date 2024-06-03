ba7b809be2be6d6c15452da96e9bf78448b59e04
Sagar Tomar <sagar.tomar@wednesday.is>
2024-06-03 18:23:58 +0530

    Updated keploy CI

--- .github/workflows/test_workflows/keploy.sh
+++ .github/workflows/test_workflows/keploy.sh
@@ -11,7 +11,7 @@ trap 'handle_error $LINENO' ERR
 
 # Fetch the Keploy installation script
 curl --silent -O -L https://keploy.io/ent/install.sh 
-export KEPLOY_API_KEY=Kca1IAlh+GKnXPaceA==
+export KEPLOY_API_KEY=Iba1IAlh+GKnXPzYeA==
 
 curl --silent -o keployE --location "https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/enterprise_linux_amd64"
 
@@ -22,27 +22,26 @@ sudo chmod a+x keployE && sudo mkdir -p /usr/local/bin && sudo mv keployE /usr/l
 source ~/.bashrc
 
 # Install the required node dependencies
-yarn install
+# yarn install
+export DOCKER_BUILDKIT=1
+sudo -E docker-compose --env-file .env.docker build
 
 export ENVIRONMENT_NAME=local
-
 # Build the project locally
 sudo -E env PATH="$PATH" yarn build:local
 echo "Project built successfully"
 
 # Start keploy in test mode
-sudo -E env PATH="$PATH" keployE test -c 'sh ./scripts/migrate-and-run.sh' --delay 50 --generateGithubActions=false
+sudo -E env PATH="$PATH" keployE  test -c "sudo docker-compose --env-file .env.docker up" --containerName "custom_app" --delay 50 --apiTimeout 300 --generateGithubActions=false --coverage=false
 echo "Keploy started in test mode"
 
-report_file="./keploy/reports/test-run-0/test-set-0-report.yaml"
+report_file="./keploy/reports/test-run-0/test-set-4-report.yaml"
 test_status1=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
-
+echo test_status1
 # Check the test status variables and exit accordingly
 # if [ "$test_status1" = "PASSED" ]; then
-npx nyc report --reporter=lcov 
 echo "All tests passed"
-exit 0
+# exit 0
 # else
-#     echo "Some tests failed"
 #     exit 1
 # fi
