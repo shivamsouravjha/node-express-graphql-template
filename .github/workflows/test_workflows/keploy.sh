1fd8d880e9a4ce9f9b6b84438ec3f67b08b8bd41
Sarthak160 <rocksarthak45@gmail.com>
2024-06-09 13:04:35 +0000

    workflow check
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>

--- .github/workflows/test_workflows/keploy.sh
+++ .github/workflows/test_workflows/keploy.sh
@@ -1,27 +1,27 @@
 curl --silent -O -L https://keploy.io/ent/install.sh 
 
 
-sudo docker compose --env-file .env.docker build
-export KEPLOY_API_KEY=Iba1IAlh+GKnXPzYeA==
-curl --silent -o keployE --location "https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/enterprise_linux_amd64"
-sudo chmod a+x keployE && sudo mkdir -p /usr/local/bin && sudo mv keployE /usr/local/bin
+# sudo docker compose --env-file .env.docker build
+# export KEPLOY_API_KEY=Iba1IAlh+GKnXPzYeA==
+# curl --silent -o keployE --location "https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/enterprise_linux_amd64"
+# sudo chmod a+x keployE && sudo mkdir -p /usr/local/bin && sudo mv keployE /usr/local/bin
 
 
 # curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp
 
 # sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy /usr/local/bin/keploybin
 # Clone the specific branch from the Keploy repository
-# git clone --branch shivam/clifix https://github.com/keploy/keploy.git /tmp/keploy_repo
-# cd /tmp/keploy_repo
+git clone --branch fix-racePg https://github.com/keploy/keploy.git /tmp/keploy_repo
+cd /tmp/keploy_repo
 
 # # # Build the binary
-# go build  -tags=viper_bind_struct -o keploybin .
+go build -race -tags=viper_bind_struct -o keploybin .
 # # # Move the binary to /usr/local/bin
-# sudo mkdir -p /usr/local/bin && sudo mv keploybin /usr/local/bin/keploybin
+sudo mkdir -p /usr/local/bin && sudo mv keploybin /usr/local/bin/keploybin
 
-# sudo docker build -t ghcr.io/keploy/keploy:v2-dev .
+sudo docker build -t ghcr.io/keploy/keploy:v2-dev .
 
-# cd -
+cd -
 
 
 # Run the Keploy installation script
@@ -34,7 +34,7 @@ export ENVIRONMENT_NAME=local
 echo "Project built successfully"
 
 # Start keploy in test mode
-sudo -E env PATH="$PATH" /usr/local/bin/keployE test -c "sudo docker compose --env-file .env.local up" --containerName "custom_app" --delay 250 --buildDelay 150 --apiTimeout 300 --generateGithubActions=false --coverage=false
+sudo -E env PATH="$PATH" /usr/local/bin/keploybin test -c "sudo docker compose --env-file .env.local up" --containerName "custom_app" --delay 250 --buildDelay 150 --apiTimeout 300 --generateGithubActions=false --coverage=false
 echo "Keploy started in test mode"
 
 # Initialize a flag to track the overall test status
