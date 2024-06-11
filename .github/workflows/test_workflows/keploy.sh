e95607418cdbfe5e4084c9d27ea7c94b00b9c5f5
shivamsouravjha <shivamsouravjha@gmail.com>
2024-06-11 12:40:42 +0530

    fix:  integrate latest release changes
    Signed-off-by: shivamsouravjha <shivamsouravjha@gmail.com>

--- .github/workflows/test_workflows/keploy.sh
+++ .github/workflows/test_workflows/keploy.sh
@@ -1,47 +1,66 @@
-#!/bin/bash
-
-# Debugging function to handle errors
-handle_error() {
-    echo "Error on line $1"
-    exit 1
-}
-
-# Trap errors and call the handle_error function
-trap 'handle_error $LINENO' ERR
-
-# Fetch the Keploy installation script
 curl --silent -O -L https://keploy.io/ent/install.sh 
+
+
+sudo docker compose -f keploy-docker-compose.yml build
 export KEPLOY_API_KEY=Iba1IAlh+GKnXPzYeA==
-
 curl --silent -o keployE --location "https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/enterprise_linux_amd64"
-
 sudo chmod a+x keployE && sudo mkdir -p /usr/local/bin && sudo mv keployE /usr/local/bin
 
-# Run the Keploy installation script
-# bash ./install.sh
-source ~/.bashrc
+
+# curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp
+
+# sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy /usr/local/bin/keploybin
+# git clone --branch fix-racePg https://github.com/keploy/keploy.git /tmp/keploy_repo
+# cd /tmp/keploy_repo
+
+# # # # Build the binary
+# go build -race -tags=viper_bind_struct -o keploybin .
+# # # # Move the binary to /usr/local/bin
+# sudo mkdir -p /usr/local/bin && sudo mv keploybin /usr/local/bin/keploybin
+
+# sudo docker build -t ghcr.io/keploy/keploy:v2-dev .
+
+# cd -
+
+
 
 # Install the required node dependencies
 # yarn install
-export DOCKER_BUILDKIT=1
-sudo -E docker-compose --env-file .env.docker build
 
 export ENVIRONMENT_NAME=local
 # Build the project locally
-sudo -E env PATH="$PATH" yarn build:local
 echo "Project built successfully"
 
-# Start keploy in test mode
-sudo -E env PATH="$PATH" keployE  test -c "sudo docker-compose --env-file .env.docker up" --containerName "custom_app" --delay 50 --apiTimeout 300 --generateGithubActions=false --coverage=false
+
+sudo -E env PATH="$PATH" /usr/local/bin/keployE test -c "sudo docker compose -f keploy-docker-compose.yml up" --containerName "custom_app" --delay 30 --apiTimeout 300 --generateGithubActions=false --coverage=false
 echo "Keploy started in test mode"
 
-report_file="./keploy/reports/test-run-0/test-set-4-report.yaml"
-test_status1=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
-echo test_status1
-# Check the test status variables and exit accordingly
-# if [ "$test_status1" = "PASSED" ]; then
-echo "All tests passed"
-# exit 0
-# else
-#     exit 1
-# fi
+all_passed=true
+
+# Loop through test sets 1 to 4
+for i in {1..4}
+do
+    # Define the report file for each test set
+    report_file="./keploy/reports/test-run-0/test-set-$i-report.yaml"
+
+    # Extract the test status
+    test_status=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
+
+    # Print the status for debugging
+    echo "Test status for test-set-$i: $test_status"
+
+    # Check if any test set did not pass
+    if [ "$test_status" != "PASSED" ]; then
+        all_passed=false
+        echo "Test-set-$i did not pass."
+        break # Exit the loop early as all tests need to pass
+    fi
+done
+
+# Check the overall test status and exit accordingly
+if [ "$all_passed" = true ]; then
+    echo "All tests passed"
+    exit 0
+else
+    exit 1
+fi
--- Dockerfile
+++ Dockerfile
@@ -17,16 +17,16 @@ ARG ENVIRONMENT_NAME
 ARG BUILD_NAME
 ARG APP_PATH
 ENV APP_PATH=${APP_PATH:-/default/path} 
-RUN yarn global add sequelize-cli@6.2.0 nyc
+RUN yarn global add sequelize-cli@6.2.0 nyc@15.1.0
 RUN yarn add shelljs bull dotenv pg sequelize@6.6.5
 RUN apk add --no-cache dumb-init
 ADD scripts/migrate-and-run.sh ${APP_PATH}/
 ADD package.json ${APP_PATH}/
 ADD . ${APP_PATH}/
 COPY --from=0 ${APP_PATH}/dist ${APP_PATH}/dist
-ADD https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/assets/freeze_time_arm64.so /lib/keploy/freeze_time_arm64.so
-RUN chmod +x /lib/keploy/freeze_time_arm64.so
-ENV LD_PRELOAD=/lib/keploy/freeze_time_arm64.so
+ADD https://keploy-enterprise.s3.us-west-2.amazonaws.com/releases/latest/assets/freeze_time_amd64.so /lib/keploy/freeze_time_amd64.so
+RUN chmod +x /lib/keploy/freeze_time_amd64.so
+ENV LD_PRELOAD=/lib/keploy/freeze_time_amd64.so
 
 
 # Set working directory
